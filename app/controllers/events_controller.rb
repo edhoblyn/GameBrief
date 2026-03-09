b31bef416bbb46b3f0de66cbee2e6c208ae079f6
Baptistesrd <baptiste.sardou@edhec.com>
2026-03-09 13:46:54 +0000

    person4

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.order(start_date: :asc)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :favourites, dependent: :destroy
+  has_many :favourite_games, through: :favourites, source: :game
+  has_many :reminders, dependent: :destroy
 end
