581e840ce2b9fbca1525cbb93e149ffa5d324133
edhoblyn <edhoblyn@gmail.com>
2026-03-09 11:24:40 +0000

    added views/games/index.html.erb and show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :games
+  resources :games, only: [:index, :show]
+
   root "games#index"
 end
