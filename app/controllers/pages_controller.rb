4cf3b3e46c1496c5f59f8b39766dde96c5eb8788
Baptistesrd <baptiste.sardou@edhec.com>
2026-03-10 14:55:04 +0000

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,12 @@ class PagesController < ApplicationController
     @featured_gamers = User.where.not(username: nil).order(follower_count: :desc).limit(4)
   end
 
-  def dashboard
+  def my_profile
     @followed_games = current_user.favourite_games
     @reminders = current_user.reminders.includes(event: :game).order("events.start_date asc")
+    @recent_patches = Patch.where(game: @followed_games)
+                           .order(created_at: :desc)
+                           .limit(10)
+                           .includes(:game)
   end
 end
