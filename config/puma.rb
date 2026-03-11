97b990b3d92f5ca430a1799eb323d2c5edefb1dd
edhoblyn <edhoblyn@gmail.com>
2026-03-11 13:48:18 +0000

    Fix production patch scraping by enabling Solid Queue on Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -34,8 +34,9 @@ port ENV.fetch("PORT", 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# Run the Solid Queue supervisor inside Puma by default in production so
+# recurring jobs execute on single-dyno platforms like Heroku.
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || ENV.fetch("RAILS_ENV", nil) == "production"
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
