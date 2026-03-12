6ddc35d727dae5a440629958a28be805707052c0
edhoblyn <edhoblyn@gmail.com>
2026-03-12 20:46:14 +0000

    Style follow/unfollow toasts with green and red theme colours

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,14 +3,14 @@ class FavouritesController < ApplicationController
 
   def create
     @favourite = current_user.favourites.find_or_create_by(game_id: params[:game_id])
-    flash[:notice] = "Game added to your favourites!"
+    flash[:success] = "Game added to your favourites!"
     redirect_back fallback_location: root_path
   end
 
   def destroy
     @favourite = current_user.favourites.find(params[:id])
     @favourite.destroy
-    flash[:notice] = "Game removed from your favourites."
+    flash[:danger] = "Game removed from your favourites."
     redirect_back fallback_location: root_path
   end
 end
