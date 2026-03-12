7225814ce59c0157643f818f4f98d53cc4bbecda
edhoblyn <edhoblyn@gmail.com>
2026-03-12 11:30:55 +0000

    Restore missing profile pages, toggles, and footer links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @featured_gamers = User.where.not(username: nil).order(follower_count: :desc).limit(12)
+    @featured_gamers = User.where.not(username: nil).order(follower_count: :desc).limit(4)
   end
 
   def find_friends
@@ -20,5 +20,30 @@ class PagesController < ApplicationController
                            .recent_first
                            .limit(10)
                            .includes(:game)
+  end
+
+  def my_games
+    @games = current_user.favourite_games.order(:name)
+  end
+
+  def my_patches
+    @followed_games = current_user.favourite_games
+    @date_filter = params[:date_filter].presence_in(Patch::DATE_FILTERS.keys) || "all"
+    @sort = params[:sort].presence_in(%w[newest oldest]) || "newest"
+
+    @patches = Patch.where(game: @followed_games)
+                    .includes(:game)
+                    .with_date_filter(@date_filter)
+
+    @patches = case @sort
+               when "oldest" then @patches.known_oldest_first
+               else @patches.known_newest_first
+               end
+  end
+
+  def my_events
+    @events = current_user.reminders
+                          .includes(event: :game)
+                          .order("events.start_date asc")
   end
 end
