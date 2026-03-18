3c77a2e30bb6cef0726590bd617df77ad1d54f6d
Baptistesrd <baptiste.sardou@edhec.com>
2026-03-18 14:47:41 +0000

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_18_104655) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_18_144118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -174,6 +174,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_104655) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.integer "follower_count"
+    t.string "hometown"
     t.string "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
