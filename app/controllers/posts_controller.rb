75e4dd806e3dfbe59b743fb7047c8bf552ab6782
edhoblyn <edhoblyn@gmail.com>
2026-03-19 15:38:08 +0000

    feat: real-time feed updates via Turbo Streams ActionCable broadcasting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,14 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
+      (current_user.friends.to_a + [current_user]).each do |user|
+        Turbo::StreamsChannel.broadcast_prepend_to(
+          "feed_#{user.id}",
+          target: "posts",
+          partial: "posts/post",
+          locals: { post: @post, viewer: user }
+        )
+      end
       redirect_back fallback_location: root_path
     else
       redirect_back fallback_location: root_path, alert: @post.errors.full_messages.to_sentence
